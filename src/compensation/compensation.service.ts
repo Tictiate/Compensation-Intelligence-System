fc8422f8391f040fc8c89c171d008ec676a216cc
Ishaan Gandhi <ishaangandhi2006@gmail.com>
2026-06-05 20:28:13 +0530

    fix errconnrefused

--- src/compensation/compensation.service.ts
+++ src/compensation/compensation.service.ts
@@ -1,10 +1,19 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient, Prisma } from '@prisma/client';
 import { CompareCompensationDto } from './dto/compare-compensation.dto';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import 'dotenv/config';
 
 @Injectable()
 export class CompensationService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
